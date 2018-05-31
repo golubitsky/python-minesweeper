335b603da2ddba94629235ade89d351e3a25095e
Mike Golubitsky <mike617@gmail.com>
2018-05-31 05:15:35 -0400

    board: refactor separate adj_indices method

--- src/board.py
+++ src/board.py
@@ -3,12 +3,14 @@ import os
 from math import sqrt
 from src import constants
 
+
 class Cell():
     def __init__(self):
         self.n_surrounding_mines = 0
         self.mine = False
         self.revealed = False
         self.flagged = False
+
 
 class Board():
     def __init__(self, size, n_mines=3):
@@ -17,19 +19,20 @@ class Board():
         self._insert_values_into_board()
 
     def _create_board_with_mines(self, size, n_mines):
-        board = [None for x in range(size ** 2)]
-        mine_indices = set(random.sample(range(len(board)), n_mines))        
+        board = [None for x in range(size**2)]
+        mine_indices = set(random.sample(range(len(board)), n_mines))
         for i in range(len(board)):
             board[i] = Cell()
             if i in mine_indices:
                 board[i].mine = True
 
         return board
-    
+
     def _insert_values_into_board(self):
         for i in range(len(self.board)):
             if not self.board[i].mine:
-                self.board[i].n_surrounding_mines = self._count_adjacent_mines(i)
+                self.board[i].n_surrounding_mines = self._count_adjacent_mines(
+                    i)
 
     def _is_in_bounds(self, y, x):
         if y < 0 or y >= self._size:
@@ -38,19 +41,27 @@ class Board():
             return False
         return True
 
-    def _count_adjacent_mines(self, index):
-        mine_count = 0
-
+    def _get_adjacent_indices(self, index):
         y, x = self._to_coord(index)
+        indices = []
         for vec_y, vec_x in constants.directions:
             adj_y, adj_x = y + vec_y, x + vec_x
             if not self._is_in_bounds(adj_y, adj_x):
                 continue
-            
+
             index = self._from_coord(adj_y, adj_x)
-            if self.board[index].mine:
+            indices.append(index)
+
+        return indices
+
+    def _count_adjacent_mines(self, index):
+        adjacent_indices = self._get_adjacent_indices(index)
+        mine_count = 0
+
+        for adj_index in adjacent_indices:
+            if self.board[adj_index].mine:
                 mine_count += 1
-        
+
         return mine_count
 
     def _to_coord(self, index):
@@ -61,14 +72,14 @@ class Board():
             Bottom-right corner = (size - 1, size - 1)
         """
         return (index % self._size, index // self._size)
-    
+
     def _from_coord(self, y, x):
         """
             The 2d board is stored as a 1d array.
             This function returns the index in the board given x and y.
         """
         return self._size * y + x
-    
+
     def reveal(self, y, x):
         """
             Reveal a cell at coord (y, x)
@@ -84,8 +95,12 @@ class Board():
         cell.flagged = not cell.flagged
 
     def lost(self):
-        return any(mine.revealed for mine in filter(lambda cell: cell.mine, self.board))
-    
+        return any(
+            mine.revealed
+            for mine in filter(lambda cell: cell.mine, self.board))
+
     def won(self):
         # each non-mine is revealed
-        return all(non_mine.revealed for non_mine in filter(lambda cell: not cell.mine, self.board))
+        return all(
+            non_mine.revealed
+            for non_mine in filter(lambda cell: not cell.mine, self.board))
