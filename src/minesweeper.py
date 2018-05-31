9de0307a5422896f8beeb6055b11419581c88019
Mike Golubitsky <mike617@gmail.com>
2018-05-31 04:48:04 -0400

    more robust input retry logic

--- src/minesweeper.py
+++ src/minesweeper.py
@@ -3,7 +3,8 @@ d = os.path.join(os.path.dirname(__file__), '..')
 sys.path.append(d)
 from src.board import Board
 from src.view import View
-from src import user_input
+from src import user_input, constants
+
 
 def play_game():
     board = Board(10)
@@ -12,11 +13,12 @@ def play_game():
     while True:
         view.print()
         result = user_input.determine_operation(retry_reason)
-        if 'retry_user_input' in result:
-            retry_reason = result['retry_user_input']
+        if constants.retry_user_input in result:
+            retry_reason = result[constants.retry_user_input]
         else:
             retry_reason = None
+            board.toggle_flag
+
 
 if __name__ == "__main__":
     play_game()
-
--- src/user_input.py
+++ src/user_input.py
@@ -1,14 +1,18 @@
 import re
+from src import constants
+
 
 def determine_operation(retry_reason):
     if retry_reason:
-        r = {'fail': general_fail, 'help': print_help}
-        r[retry_reason]()
-        
+        {
+            constants.print_fail: fail,
+            constants.print_help: print_help
+        }[retry_reason]()
+
     x = y = 0
     captured = input('Type command or (h)elp:')
     if re.search('h', captured):
-        return {'retry_user_input': 'help'}
+        return {constants.retry_user_input: constants.print_help}
 
     coords = re.findall('\d+', captured)
     if coords and len(coords) == 2:
@@ -16,20 +20,24 @@ def determine_operation(retry_reason):
 
     flag_mine = re.search('f', captured)
     reveal_cell = re.search('r', captured)
-    
+
     if flag_mine and x and y:
         return {operation: 'flag', x: x, y: y}
     elif reveal_cell and x and y:
         print(f'Going to reveal {x}, {y}')
     else:
-        return {'retry_user_input': 'fail'}
+        return {constants.retry_user_input: constants.print_fail}
 
-    
-def general_fail():
+
+def fail():
     print("Invalid input, try again or use 'h' for help")
-    
+
+
 def print_help():
-    print('Specify x and y coordinates (in x,y order) to execute operation on.')
+    print(
+        'Specify x and y coordinates (in x,y order) to execute operation on.')
     print('Acceptable operations are f (flag) and r (reveal).')
     print("Acceptable input: '3,1,f' or '2 4 r' or 'r 2 4'")
-    print('Visit https://en.wikipedia.org/wiki/Minesweeper_(video_game) for additional info.')
+    print(
+        'Visit https://en.wikipedia.org/wiki/Minesweeper_(video_game) for additional info.'
+    )
