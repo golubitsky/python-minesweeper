8c0f8fc648fdfb0ad42239352641ace93cbd0f96
Mike Golubitsky <mike617@gmail.com>
2018-05-31 05:15:10 -0400

    sending user operations to board

--- src/minesweeper.py
+++ src/minesweeper.py
@@ -7,17 +7,17 @@ from src import user_input, constants
 
 
 def play_game():
-    board = Board(10)
-    view = View(board)
+    board = Board(10, 40)
     retry_reason = None
     while True:
+        view = View(board)
         view.print()
         result = user_input.determine_operation(retry_reason)
         if constants.retry_user_input in result:
             retry_reason = result[constants.retry_user_input]
         else:
             retry_reason = None
-            board.toggle_flag
+            getattr(board, result['operation'])(x=result['x'],y=result['y'])
 
 
 if __name__ == "__main__":
--- src/user_input.py
+++ src/user_input.py
@@ -9,7 +9,7 @@ def determine_operation(retry_reason):
             constants.print_help: print_help
         }[retry_reason]()
 
-    x = y = 0
+    x = y = None
     captured = input('Type command or (h)elp:')
     if re.search('h', captured):
         return {constants.retry_user_input: constants.print_help}
@@ -21,12 +21,13 @@ def determine_operation(retry_reason):
     flag_mine = re.search('f', captured)
     reveal_cell = re.search('r', captured)
 
-    if flag_mine and x and y:
-        return {operation: 'flag', x: x, y: y}
-    elif reveal_cell and x and y:
-        print(f'Going to reveal {x}, {y}')
-    else:
-        return {constants.retry_user_input: constants.print_fail}
+    if x is not None and y is not None:
+        if flag_mine:
+            return {'operation': 'toggle_flag', 'x': x, 'y': y}
+        elif reveal_cell:
+            return {'operation': 'reveal', 'x': x, 'y': y}
+
+    return {constants.retry_user_input: constants.print_fail}
 
 
 def fail():
@@ -36,7 +37,7 @@ def fail():
 def print_help():
     print(
         'Specify x and y coordinates (in x,y order) to execute operation on.')
-    print('Acceptable operations are f (flag) and r (reveal).')
+    print('Acceptable operations are f (toggle mine flag) and r (reveal).')
     print("Acceptable input: '3,1,f' or '2 4 r' or 'r 2 4'")
     print(
         'Visit https://en.wikipedia.org/wiki/Minesweeper_(video_game) for additional info.'
